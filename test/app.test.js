d132a438c2b23145b35ee0eef46557e25bbdeffa
Vishnukumar777 <vishnuvps777@gmail.com>
2026-02-28 15:15:54 +0530

    Added .gitignore

---
+++ test/app.test.js
@@ -0,0 +1,9 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('GET /', () => {
+  it('should return success message', async () => {
+    const res = await request(app).get('/');
+    expect(res.text).toBe('CI/CD Pipeline Working!');
+  });
+});
